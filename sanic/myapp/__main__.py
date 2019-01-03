a928911e15cec7819adca859cec2dae9d80e1b7c
kanziw <kanziwoong@gmail.com>
2019-01-03 10:18:16 +0900

    [sanic] Test various request data /w postman

--- sanic/myapp/__main__.py
+++ sanic/myapp/__main__.py
@@ -1,6 +1,6 @@
 from sanic import Sanic
 from sanic.log import logger
-from sanic.response import json
+from sanic.response import json, text
 
 from .config import config
 
@@ -8,13 +8,65 @@ app = Sanic()
 app.config.update(**config)
 
 
-@app.route('/')
+@app.route("/")
 async def hello(request):
-    logger.info('Here is your info')
-    logger.warning('Here is your warning')
-    logger.error('Here is your error')
-    return json({'hello': 'world'})
+    logger.info("Here is your info")
+    logger.warning("Here is your warning")
+    logger.error("Here is your error")
+    return json({"hello": "world"})
 
 
-if __name__ == '__main__':
+@app.route("/json")
+def post_json(request):
+    return json({
+        "received": True,
+        "message": request.json,
+    })
+
+
+@app.route("/query_string")
+def query_string(request):
+    return json({
+        "parsed": True,
+        "args": request.args,
+        "url": request.url,
+        "query_string": request.query_string,
+    })
+
+
+@app.route("/files")
+def post_json(request):
+    test_file = request.files.get("test")
+
+    file_parameters = {
+        "body": len(test_file.body),
+        "name": test_file.name,
+        "type": test_file.type,
+    }
+
+    return json({
+        "received": True,
+        "file_names": request.files.keys(),
+        "test_file_parameters": file_parameters,
+    })
+
+
+@app.route("/form")
+def post_json(request):
+    return json({
+        "received": True,
+        "form_data": request.form,
+        "test": request.form.get("test"),
+    })
+
+
+@app.route("/users", methods=["POST", ])
+def create_user(request):
+    return text(
+        "You are trying to create a user with the following POST: %s"
+        % request.body
+    )
+
+
+if __name__ == "__main__":
     app.run(**app.config.SANIC_RUN)
